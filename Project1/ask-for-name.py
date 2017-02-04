e31d9b009946809bd4b67a006b6c42d2dddb5cb3
Remzi Arpaci-Dusseau <remzi.arpacidusseau@gmail.com>
2017-02-04 16:05:55 -0600

    first project for kids: just gets input and makes a decision

---
+++ Project1/ask-for-name.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+# name is a 'variable':
+# a container for some stuff we want to keep around
+user_name = raw_input("What is your name? ")
+
+if user_name == 'Maddy':
+    print 'You are wonderful at Python!'
+else:
+    print 'You stink at Python! Go ask your dad or mom for help'
+
+
+
+
+
+
+
+
